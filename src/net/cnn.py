6a75d1fca42e03948b695e5c9be856bd0384dec8
Marypi02 <m.sorrentino2811@gmail.com>
2025-12-07 19:54:26 +0100

    Aumento del padding

--- src/net/cnn.py
+++ src/net/cnn.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 class ConvBlock(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size=3, pool_size=2, **_):
         super(ConvBlock, self).__init__()
-        self.conv = nn.Conv1d(in_channels, out_channels, kernel_size=kernel_size, padding=1)
+        self.conv = nn.Conv1d(in_channels, out_channels, kernel_size=kernel_size, padding=2)
         self.relu = nn.ReLU()
         self.pool = nn.MaxPool1d(kernel_size=pool_size, stride=pool_size)
 
@@ -23,7 +23,7 @@ class DeconvBlock(nn.Module):
             out_channels, 
             kernel_size=kernel_size, 
             stride=stride,
-            padding=0 
+            padding=2 
         )
         self.relu = nn.ReLU()
 
