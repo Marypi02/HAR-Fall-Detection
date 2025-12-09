edfe386ccf04742e131d4fd9429b046e0b4a6f31
Marypi02 <m.sorrentino2811@gmail.com>
2025-12-09 20:06:57 +0100

    Fix pt2

--- src/net/cnn.py
+++ src/net/cnn.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 class ConvBlock(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size=3, pool_size=2, **_):
         super(ConvBlock, self).__init__()
-        self.conv = nn.Conv1d(in_channels, out_channels, kernel_size=kernel_size, stride=pool_size, padding=kernel_size // 2)
+        self.conv = nn.Conv1d(in_channels, out_channels, kernel_size=kernel_size, padding=kernel_size // 2)
         self.bn = nn.BatchNorm1d(out_channels)
         self.relu = nn.ReLU()
         self.pool = nn.MaxPool1d(kernel_size=pool_size, stride=pool_size)
